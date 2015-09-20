266a14bab9eb26a2131086c0f0686e3e62e047f9
yifeiwu <yiferic@gmail.com>
2015-09-20 14:58:40 +1200

    title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+
+
+  module ApplicationHelper
+	  def title(text)
+	    content_for :title, text
+	  end
+
+	  def meta_tag(tag, text)
+	    content_tag :"meta_#{tag}", text
+	  end
+
+	  def yield_meta_tag(tag, default_text='')
+	    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+	  end
+	end
+
 end
