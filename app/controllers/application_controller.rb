8a65624312082c93d13b58fff343093d7a028cf4
yifeiwu <yiferic@gmail.com>
2015-09-23 09:05:43 +1200

    checkpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
 
 
-  module ApplicationHelper
-	  def title(text)
-	    content_for :title, text
-	  end
-
-	  def meta_tag(tag, text)
-	    content_tag :"meta_#{tag}", text
-	  end
-
-	  def yield_meta_tag(tag, default_text='')
-	    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
-	  end
-	end
-
 end
