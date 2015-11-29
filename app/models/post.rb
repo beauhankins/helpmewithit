5263cdf2a761b58774abc91ee45391297587d812
Seb Patane <seb@libero.com.au>
2015-11-29 12:28:26 +1000

    Add default approved value and remove nomethoderror (weird?)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
   geocoded_by :address #can also be an IP address
-  after_validation :geocode, :if => :address_changed?
+  # after_validation :geocode, :if => :address_changed?
 
 	include PgSearch
 
